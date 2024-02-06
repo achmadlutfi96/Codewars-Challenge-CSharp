f7375ef0b10c47555888147fc95d164894cffe9b
Achmad Lutfi <alhacker1996@gmail.com>
2024-02-06 08:28:51 +0700

    Add project files.

---
+++ 8kyuu/001_Even_or_Odd.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8kyuu
+{
+    internal class _001_Even_or_Odd
+    {
+
+    }
+}
